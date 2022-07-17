5b27c9ec192aabfc2b8ff529ee98938e689eda4a
Manuel Galindez <manuelgalindez81@gmail.com>
2022-07-17 14:55:16 -0500

    Convert the functions to arrow function

--- src/extension.js
+++ src/extension.js
@@ -1,5 +1,5 @@
-const vscode = require('vscode');
-const QRCode = require('qrcode-generator');
+const vscode = require("vscode");
+const QRCode = require("qrcode-generator");
 
 const editor = vscode.window.activeTextEditor;
 
@@ -7,43 +7,38 @@ const CELL_SIZE = 5;
 const MARGIN = 8;
 const ERROR_CORRECTION_LEVEL = "L";
 
+const createQRCode = () => {
+  const selectedText = editor.document.getText(editor.selection);
+  const typeNumber = 0;
+  const qr = QRCode(typeNumber, ERROR_CORRECTION_LEVEL);
+  qr.addData(selectedText);
+  qr.make();
 
-function CreateQRCode() {
-	const selectedText = editor.document.getText(editor.selection);
-	const typeNumber = 0;
-	const qr = QRCode(typeNumber, ERROR_CORRECTION_LEVEL);
-	qr.addData(selectedText);
-	qr.make();
+  const image = qr.createImgTag(CELL_SIZE, MARGIN);
+  return image;
+};
 
-	const image = qr.createImgTag(CELL_SIZE, MARGIN);
-	return image;
-}
+const generateQRCode = () => {
+  const imageQR = createQRCode();
+  const panel = vscode.window.createWebviewPanel(
+    "qrcode",
+    "QR Preview",
+    { viewColumn: vscode.ViewColumn.Beside, preserveFocus: true },
+    {}
+  );
+  panel.webview.html = `${imageQR}`;
+};
 
-function GenerateQRCode() {
-	const imageQR = CreateQRCode();
-	const panel = vscode.window.createWebviewPanel(
-		"qrcode",
-		"QR Preview",
-		{ viewColumn: vscode.ViewColumn.Beside, preserveFocus: true },
-		{}
-	);
-	panel.webview.html = `${imageQR}`;
-	
-}
-
-function activate(context) {
-	const disposable = vscode.commands.registerCommand(
-		"qr-generator.generateQR",
-		function () {
-			GenerateQRCode();
-		}
-	);
-	context.subscriptions.push(disposable);
-}
-
-function deactivate() {}
+const activate = (context) => {
+  const disposable = vscode.commands.registerCommand(
+    "qr-generator.generateQR",
+    () => {
+      generateQRCode();
+    }
+  );
+  context.subscriptions.push(disposable);
+};
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+};
